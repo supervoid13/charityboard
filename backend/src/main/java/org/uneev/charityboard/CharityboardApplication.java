f2f21c35aac26121daf8f11e8d4adb7c21a9f68c
Ruslan Yuneev <spaceejs@gmail.com>
2024-05-05 18:28:19 +0300

    Some filename fix and add OpenAPI Docs

--- backend/src/main/java/org/uneev/charityboard/CharityboardApplication.java
+++ backend/src/main/java/org/uneev/charityboard/CharityboardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Charityboard2Application {
+public class CharityboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Charityboard2Application.class, args);
+		SpringApplication.run(CharityboardApplication.class, args);
 	}
 
 }
